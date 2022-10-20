3274df9e65413766e46d9d6e57520d5785d7b376
Maria Kazakova <mariakazakova92@gmail.com>
2022-10-20 09:47:07 +0300

    create markup login form

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export const LoginForm = () => {
   return (
     <div>
-      <form className="flex-col">
+      <div>
+        <img src="#" alt="sighIn" />
+        <h1>Sigh in</h1>
+      </div>
+      <form className="flex flex-col gap-1">
         <TextField
-          id="filled-login-input"
+          id="outlined-password-input"
           label="Login"
           type="login"
           autoComplete="current-password"
-          variant="filled"
         />
         <TextField
-          id="filled-password-input"
+          id="outlined-password-input"
           label="Password"
           type="password"
           autoComplete="current-password"
-          variant="filled"
         />
+        <div>
+          <label>
+            <input type="checkbox" id="rememberMe" name="rememberMe" />
+            <span> </span>
+            Remember Me
+          </label>
+        </div>
+        <Button variant="contained">Sigh in</Button>
       </form>
     </div>
   );
