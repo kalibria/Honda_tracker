8e9245ee038d9d0884f5d65a0f11498d5e8a03d3
Maria Kazakova <mariakazakova92@gmail.com>
2022-10-20 14:53:06 +0300

    add onSubmit on the login form

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { Formik, Form, useField, FieldHookConfig } from 'formik';
+import * as Yup from 'yup';
 import '../../App.css';
+
+interface MyTextInputProps {
+  label: string;
+  name: string;
+  [key: string]: string | FieldHookConfig<any>;
+}
+
+const MyTextInput: React.FC<MyTextInputProps> = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <TextField label={label} {...field} {...props} />
+
+      {meta.touched && meta.error ? (
+        <div className="text-red-600">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+interface MyCheckboxProps {
+  name: string;
+  [key: string]: string | FieldHookConfig<any>;
+}
+
+const MyCheckbox: React.FC<MyCheckboxProps> = ({ children, ...props }) => {
+  const [field, meta] = useField({ ...props, type: 'checkbox' });
+  return (
+    <>
+      <label>
+        <>
+          <input type="checkbox" {...field} {...props} className="mr-1.5" />
+          {children}
+        </>
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 
 export const LoginForm = () => {
   return (
@@ -14,28 +57,45 @@ export const LoginForm = () => {
           />
           <h1 className="text-center">Sigh in</h1>
         </div>
-        <form className="flex flex-col sm:w-96 space-y-3.5">
-          <TextField
-            id="outlined-login-input"
-            label="Login"
-            type="login"
-            autoComplete="current-password"
-          />
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-          />
-          <div>
-            <label>
-              <input type="checkbox" id="rememberMe" name="rememberMe" />
-              <span> </span>
-              Remember Me
-            </label>
-          </div>
-          <Button variant="contained">Sigh in</Button>
-        </form>
+        <Formik
+          initialValues={{
+            login: '',
+            password: '',
+            rememberMe: false,
+          }}
+          validationSchema={Yup.object({
+            login: Yup.string()
+              .max(15, 'Must be 15 characters or less')
+              .required('Required'),
+            password: Yup.string()
+              .max(20, 'Must be 20 characters or less')
+              .required('Required'),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }}>
+          <Form className="flex flex-col sm:w-96 space-y-3.5">
+            <MyTextInput
+              id={'outlined-login-input'}
+              label={'Login'}
+              type={'login'}
+              autoComplete={'current-login'}
+              name={'login'}
+            />
+            <MyTextInput
+              id={'outlined-password-input'}
+              label={'Password'}
+              type={'password'}
+              autoComplete={'current-password'}
+              name={'password'}
+            />
+            <MyCheckbox name={'rememberMe'}>Remember me</MyCheckbox>
+            <Button variant="contained" type="submit">
+              Sigh in
+            </Button>
+          </Form>
+        </Formik>
       </main>
     </div>
   );
