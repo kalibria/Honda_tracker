e4b35f5132629d0176a0c564206d58fc58707a04
Maria Kazakova <mariakazakova92@gmail.com>
2022-11-17 15:05:45 +0300

    replaced navigate to redirect in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React, { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, redirect, useLocation, useNavigate } from 'react-router-dom';
 import ButtonAppBar from 'src/appBar/ButtonAppBar';
 import { welcomePath } from 'src/router/rootConstants';
 import { myLocalStorage } from 'src/services/localStorage';
 
 export const App = () => {
   const isAuth = myLocalStorage.isAuth();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {}, [location]);
 
   useEffect(() => {
     if (isAuth) {
       // navigate(calendarPath);
     } else {
-      navigate(welcomePath);
+      redirect(welcomePath);
     }
-  }, [isAuth, navigate]);
+  }, [isAuth]);
 
   return (
     <React.Fragment>
