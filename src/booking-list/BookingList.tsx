82ef783b0742b9d07c1c0af085064572f6e2f2fd
Maria Kazakova <mariakazakova92@gmail.com>
2022-11-21 16:02:11 +0300

    added dates list in UI

--- src/booking-list/BookingList.tsx
+++ src/booking-list/BookingList.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { datesManager } from 'src/booking-list/datesManager';
 
 export const BookingList = () => {
-  const datesList = datesManager.getFormattingDates();
-  return (
-    <div>
-      <div>Calendar</div>
-    </div>
-  );
+  const arrFormattingDates = datesManager.getFormattingDates();
+  const datesList = arrFormattingDates.map((date, index) => {
+    return <li key={index}>{date}</li>;
+  });
+
+  return <ul>{datesList}</ul>;
 };
