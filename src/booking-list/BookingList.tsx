cbc577a42395e0275677a40d91c9ea11270e68cc
Maria Kazakova <mariakazakova92@gmail.com>
2022-11-22 11:02:52 +0300

    add styles (grid) to the booking list

--- src/booking-list/BookingList.tsx
+++ src/booking-list/BookingList.tsx
@@ -4,8 +4,20 @@ import { datesManager } from 'src/booking-list/datesManager';
 export const BookingList = () => {
   const arrFormattingDates = datesManager.getFormattingDates();
   const datesList = arrFormattingDates.map((date, index) => {
-    return <li key={index}>{date}</li>;
+    return (
+      <li className={'cellDecoration'} key={index}>
+        {date}
+      </li>
+    );
   });
 
-  return <ul>{datesList}</ul>;
+  return (
+    <div className={'bookingPage'}>
+      <div className={'bookingWrapper'}>
+        <div className={'bookingHeader cellDecoration'}>Booking list</div>
+        <ul>{datesList}</ul>
+        <ul>booking time</ul>
+      </div>
+    </div>
+  );
 };
