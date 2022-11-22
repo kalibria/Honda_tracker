a39e842f5c0ea403ae2ede92684abef097ead5fe
Maria Kazakova <mariakazakova92@gmail.com>
2022-11-22 12:03:19 +0300

    code refactoring.The date items were moved to a separate file

---
+++ src/booking-list/DateItems.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { datesManager } from 'src/booking-list/datesManager';
+
+export const DateItems = () => {
+  const arrFormattingDates = datesManager.getFormattingDates();
+  const dateItems = arrFormattingDates.map((date, index) => {
+    return (
+      <li className={'cellDecoration'} key={index}>
+        {date}
+      </li>
+    );
+  });
+
+  return <ul>{dateItems}</ul>;
+};
