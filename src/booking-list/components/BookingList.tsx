95a72352e3c215949d77826b0142c6c00d8a87b9
Maria Kazakova <mariakazakova92@gmail.com>
2022-12-15 14:52:45 +0300

    if user role is 'driver' he will see 'создать поездку' button

--- src/booking-list/components/BookingList.tsx
+++ src/booking-list/components/BookingList.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { CalendarItemsWrapper } from 'src/booking-list/components/CalendarItemsWrapper';
+import { driverRole } from 'src/booking-list/constants';
 import { ButtonUI } from 'src/commonComponents/ButtonUI';
+import { RootState } from 'src/redux/store';
 
 export const BookingList = () => {
+  const selectUserRole = useSelector((state: RootState) => state.userData.role);
+
+  const isDriverRole = selectUserRole.includes(driverRole);
   const createNewRide = () => {};
 
   return (
     <div className={'bookingPage'}>
-      <div className={'button'}>
-        <ButtonUI onClick={createNewRide}>{'Создать поездку'}</ButtonUI>
-      </div>
+      {isDriverRole && (
+        <div className={'button'}>
+          <ButtonUI onClick={createNewRide}>{'Создать поездку'}</ButtonUI>
+        </div>
+      )}
       <div className={'bookingWrapper'}>
-        <div className={'bookingHeader cellDecoration'}>Booking list</div>
+        <div className={'bookingHeader cellDecoration'}>Список поездок</div>
         <CalendarItemsWrapper />
       </div>
     </div>
--- src/settings/SettingsPage.tsx
+++ src/settings/SettingsPage.tsx
@@ -1,10 +1,8 @@
 import Button from '@mui/material/Button';
 import { Form, Formik } from 'formik';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { RootState } from 'src/redux/store';
-import { loginPath } from 'src/router/rootConstants';
 import { myLocalStorage } from 'src/services/localStorage';
 import { BasicTextFields, SwitchesGroup } from 'src/settings/components';
 import { carProvider } from 'src/settings/constants';
@@ -12,16 +10,8 @@ import { carProvider } from 'src/settings/constants';
 export const SettingsPage = () => {
   const isAuth = myLocalStorage.isAuth();
   const selectMyRole = useSelector((state: RootState) => state.userData.role);
-  const navigate = useNavigate();
 
   const isCarProvider = selectMyRole.includes(carProvider);
-  const { pathname } = useLocation();
-
-  // useEffect(() => {
-  //   if (!isAuth) {
-  //     navigate(loginPath, { state: pathname });
-  //   }
-  // });
 
   return (
     <div className={'sm:w-60 mainContainer'}>
