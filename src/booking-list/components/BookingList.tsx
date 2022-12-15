469b675770c59ed70081d64124723b10c83ef442
Maria Kazakova <mariakazakova92@gmail.com>
2022-12-15 14:32:38 +0300

    added 'Создать поездку' button on the BookingList page

--- src/booking-list/components/BookingList.tsx
+++ src/booking-list/components/BookingList.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { CalendarItemsWrapper } from 'src/booking-list/components/CalendarItemsWrapper';
+import { ButtonUI } from 'src/commonComponents/ButtonUI';
 
 export const BookingList = () => {
+  const createNewRide = () => {};
+
   return (
     <div className={'bookingPage'}>
+      <div className={'button'}>
+        <ButtonUI onClick={createNewRide}>{'Создать поездку'}</ButtonUI>
+      </div>
       <div className={'bookingWrapper'}>
         <div className={'bookingHeader cellDecoration'}>Booking list</div>
         <CalendarItemsWrapper />
