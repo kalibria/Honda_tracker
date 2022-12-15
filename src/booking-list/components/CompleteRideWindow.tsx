7d68a03baa50f29d686ab67a05d9805ff305656c
Maria Kazakova <mariakazakova92@gmail.com>
2022-12-15 10:30:21 +0300

    create CompleateRideWindow with Formik

---
+++ src/booking-list/components/CompleteRideWindow.tsx
@@ -0,0 +1,27 @@
+import { useFormik } from 'formik';
+import React from 'react';
+
+export const CompleteRideWindow = () => {
+  const formik = useFormik({
+    initialValues: {
+      carLocation: '',
+    },
+    onSubmit: (values) => {},
+  });
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor={'carLocation'}>Местонахождение машины</label>
+        <input
+          id="carLocation"
+          name="carLocation"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.carLocation}
+        />
+        <button type="submit">Подтвердить</button>
+      </form>
+    </div>
+  );
+};
