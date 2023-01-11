4af9c5fb1085b4cad5433eaf2e712aaeb23122df
Maria Kazakova <mariakazakova92@gmail.com>
2023-01-11 15:17:27 +0300

    calendar dates are not duplicated

--- src/booking-list/components/CalendarItems.tsx
+++ src/booking-list/components/CalendarItems.tsx
@@ -4,29 +4,37 @@ import { IUICalendarItems } from 'src/booking-list/types';
 
 export const CalendarItems = ({ datesRidesForUI }: IUICalendarItems) => {
   const items = datesRidesForUI.map((item, index) => {
-    const prevItems = item.info.map((el, indexEl) => {
-      return (
-        <div className={'calendar'} key={el.id}>
-          <li className={'cellDecoration'}>{item.date}</li>
-          <li className={'cellDecoration'}>
-            {el.username ? (
-              <ul>
-                <li className={'fontBold'}>{el.username}</li>
-                <li className={'fontBold'} key={el.id}>
-                  <Link to={`/booking-details/${el.id}`}>{el.description}</Link>
-                </li>
-              </ul>
-            ) : (
-              <ul>
-                <li>{el.username}</li>
-                <li>{el.description}</li>
-              </ul>
-            )}
-          </li>
-        </div>
-      );
-    });
-    return prevItems;
+    return (
+      <ul className={'calendar'} key={item.date}>
+        {/*<ul className={'cellDecoration'}>*/}
+        {/*  {item.date}*/}
+        {/*</ul>*/}
+        <li className={'cellDecoration'}>{item.date}</li>
+        <ul>
+          {item.info.map((el, indexEl) => {
+            return (
+              <li key={el.id} className={'cellDecoration'}>
+                {el.username ? (
+                  <ul>
+                    <li className={'fontBold'}>{el.username}</li>
+                    <li className={'fontBold'} key={el.id}>
+                      <Link to={`/booking-details/${el.id}`}>
+                        {el.description}
+                      </Link>
+                    </li>
+                  </ul>
+                ) : (
+                  <ul>
+                    <li>{el.username}</li>
+                    <li>{el.description}</li>
+                  </ul>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </ul>
+    );
   });
 
   return <ul>{items}</ul>;
