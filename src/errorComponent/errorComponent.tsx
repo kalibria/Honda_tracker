ba5bf78352422ab411912e543ec80c8beeefdca0
Maria Kazakova <mariakazakova92@gmail.com>
2023-03-09 12:09:06 +0300

    created error component

---
+++ src/errorComponent/errorComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const errorComponent = () => {
+  return (
+    <div>
+      <img
+        src={'src/errorComponent/errorComponent.tsx'}
+        alt={'Oops, something went wrong....'}
+      />
+    </div>
+  );
+};
