064bd959adddd2e725d712b304594262e3668057
Maria Kazakova <mariakazakova92@gmail.com>
2023-03-09 11:17:39 +0300

    Welcome page is displayed on the first visit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import React, { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import ButtonAppBar from 'src/appBar/ButtonAppBar';
 import { useIsIdTokenExpired } from 'src/auth/authenticationManager';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { welcomePath } from 'src/router/rootConstants';
+import { myLocalStorage } from 'src/services/localStorage';
+import { WelcomeToHondaTracker } from 'src/welcom/WelcomeToHondaTracker';
 
 export const App = () => {
   useIsIdTokenExpired();
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    navigate(welcomePath);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const isRefreshToken = myLocalStorage.getItem('RefreshToken');
 
   return (
     <div>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <ButtonAppBar />
+        {!isRefreshToken && <WelcomeToHondaTracker />}
         <Outlet />
       </LocalizationProvider>
     </div>
